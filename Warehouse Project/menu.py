e8f3033cf407e1bfb72f0421045ae75777e0817f
LizWilliamson <53950391+LizWilliamson@users.noreply.github.com>
2020-01-11 20:32:05 -0800

    Add files via upload

---
+++ Warehouse Project/menu.py
@@ -0,0 +1,20 @@
+def print_menu():
+    print("*" * 55)
+    print("Olivander's Wand Shop Inventory Control System")
+    print("*" * 55)
+
+    print("[1] Register New Item")
+    print("[2] List All Items")
+    print("[3] Update Stock")
+    print("[4] List Items With No Stock")
+
+    print("[5] Remove Item")
+    print("[6] Print Categories")
+    print("[7] Print stock value")
+
+    print("[8] Register Purchase")
+    print("[9] Register a Sell")
+
+    print("[10] See log of events")
+
+    print("[x] Exit")
